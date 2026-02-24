90267a5e07e17374fa5ca87bb262cf7fb23137c1
JorgeMrj <roadtoello2@gmail.com>
2026-02-24 16:28:50 +0100

    vistas implementadas

--- PandaDawRazor/Program.cs
+++ PandaDawRazor/Program.cs
@@ -1,9 +1,60 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using PandaBack.Data;
+using PandaBack.Models;
+using PandaBack.Repositories;
+using PandaBack.Repositories.Auth;
+using PandaBack.Repository;
+using PandaBack.Services;
+using PandaBack.Services.Auth;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+// Configurar sesión
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromHours(2);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+builder.Services.AddHttpContextAccessor();
+
+// Configurar DbContext (compartido con PandaBack)
+builder.Services.AddDbContext<PandaDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Configurar Identity
+builder.Services.AddIdentity<User, IdentityRole>(options => 
+    {
+        options.Password.RequireDigit = true;
+        options.Password.RequireLowercase = true;
+        options.Password.RequiredLength = 6;
+        options.User.RequireUniqueEmail = true;
+    })
+    .AddEntityFrameworkStores<PandaDbContext>()
+    .AddDefaultTokenProviders();
+
+// Registrar repositorios de PandaBack
+builder.Services.AddScoped<IProductoRepository, ProductoRepository>();
+builder.Services.AddScoped<ICarritoRepository, CarritoRepository>();
+builder.Services.AddScoped<IFavoritoRepository, FavoritoRepository>();
+builder.Services.AddScoped<IVentaRepository, VentaRepository>();
+builder.Services.AddScoped<IValoracionRepository, ValoracionRepository>();
+builder.Services.AddScoped<IAuthRepository, AuthRepository>();
+
+// Registrar servicios de PandaBack
+builder.Services.AddScoped<IProductoService, ProductoService>();
+builder.Services.AddScoped<ICarritoService, CarritoService>();
+builder.Services.AddScoped<IFavoritoService, FavoritoService>();
+builder.Services.AddScoped<IVentaService, VentaService>();
+builder.Services.AddScoped<IValoracionService, ValoracionService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<TokenService>();
 
 var app = builder.Build();
 
@@ -11,7 +62,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -19,6 +69,9 @@ app.UseHttpsRedirection();
 
 app.UseRouting();
 
+app.UseSession();
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapStaticAssets();
